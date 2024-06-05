55b7a9116ade7719bab38032831181c4ca36bef4
anjeza <anjezagllarevaa@gmail.com>
2024-06-05 22:40:49 +0200

    added some js effects on main bg

--- projektiueb/src/components/side-components/overlay.js
+++ projektiueb/src/components/side-components/overlay.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import './Overlay.scss';
+
 const Overlay = ({ bg, color }) => {
+  useEffect(() => {
+    const overlay = document.querySelector('.overlay');
+    overlay.style.backgroundColor = color;
+    overlay.classList.add('fade-in');
+  }, [color]);
+
   return (
-    <div>
-      <img className="bg" src={bg} />
+    <div className="overlay-container">
+      <img className="bg" src={bg} alt="background" />
       <div className="overlay"></div>
     </div>
   );
 };
+
 export default Overlay;
